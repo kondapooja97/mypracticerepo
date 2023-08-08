84ce9bc2d18171029516f256ff9b7421d3be770d
kondapooja97 <kondapooja97@gmail.com>
2023-08-08 13:24:20 +0530

    slicestring

---
+++ stringmethods.js
@@ -0,0 +1,8 @@
+// String Methods
+let str="Mynameis pooja";
+let textlength=str.length;
+console.log('Length of String is:',textlength);
+
+// slice stringmethods
+let a=str.slice(4,8);
+console.log('slice of a is: ',a);

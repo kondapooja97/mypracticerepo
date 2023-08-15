5ff065ae1c332d3a2d3fb81eb0039971e57453d7
kondapooja97 <kondapooja97@gmail.com>
2023-08-15 12:14:43 +0530

    number methods

---
+++ numbermethods.js
@@ -0,0 +1,18 @@
+// number methods : All datatypes have valueOf() and toString() methods
+
+let a=500123;
+let b=a.toString();
+console.log(b);
+
+let c=parseInt(b);
+console.log(b);
+
+// tofixed
+let wt = 50.3696;
+let k = wt.toFixed(3);
+console.log(k);
+
+// valueof
+let r = 'pooja';
+h = r.valueOf(); //default keyword is var in js
+console.log(h);

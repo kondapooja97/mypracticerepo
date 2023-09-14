8d340198e6997dda900a4d168a3e990d3cb67883
kondapooja97 <kondapooja97@gmail.com>
2023-09-14 19:18:53 +0530

    one way data binding

--- Myproject/src/app/app.component.ts
+++ Myproject/src/app/app.component.ts
@@ -19,13 +19,54 @@ export class AppComponent { //class to write code here
  name='Aranya';
  num1=10;
  num2=100;
+ no3='88';
 
+ cars:string[]=['Audi','Jaguar','swift','BMW'];  //array of property
+ car=this.cars[2];
+
+ student={name:'pooja',age:26,salary:35000};
+
+ genderM='Male';
+ genderF='Female';
+
+ paintinghobbies="painting";
+ sketchinghobbies="sketching"
+ readinghobbies="reading";
+
+  userhobbies:string[]=[];
+
+  favfruit = 'Orange';
   constructor(){}    //to inject dependancies
   
-//life cycle hooks 
+    //life cycle hooks 
 
-//functions
-  learningstringinterpolation(){
+    //functions
+    //Data binding: It is a technique to link data between a component and the DOM. 
+    //1.one way data binding 2.two way data binding
+    //1.one way data binding :1.string interpolation 2.property binding 3.event binding
+    //string interpolation:It is a one way data binding technique: .ts/model to dom/html/template {{}}
+
+  learningstringinterpolation(){ //Void: default type of function 
       return this.city2;
   }
+   
+  gen(data:any){
+    console.log('data',data.target.value);
+    
+   }
+
+  Hobbies(data : any){
+    console.log(data.target.value);
+    console.log(this.userhobbies);
+   }
+   //this keyword use >> js : this key word is getting used inside object to point object's key
+  //ts: this keyword is used to point global properties.
+  //It is also used in functions to fetch value of global properties.
+  
+   fruit(){
+    console.log(this.favfruit);
+   }
+  
+  
+
 }
